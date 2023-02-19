14b3fa26b295aec9017c260b17d1598aa5595b82
qweOkk <647038923@qq.com>
2023-02-19 23:05:51 +0800

    New version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,27 @@
 package org.example;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.Objects;
+
 public class Main extends Application{
 
     public static void main(String[] args){
         launch(args);
     }
-    public void start(Stage FirstStage) throws Exception{
+    public void start(Stage args0) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("Scene/Login.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        args0.setScene(scene);
+        args0.setTitle("Login");
+        args0.show();
 
-        Pane pane =new Pane();
-        FirstStage.setScene(new Scene(pane,640,480));
-        FirstStage.show();
     }
 }
